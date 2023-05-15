359408fd529f99cd577d2492760da8c1acfe7d69
zairasssilveira <zaira.salomao12@gnail.com>
2023-05-15 10:59:33 -0300

    Initial commit

---
+++ heart.py
@@ -0,0 +1,21 @@
+import Math
+from turtle import *
+def hearta (k)
+    return 12 * Math.sin**3
+def heart(k):
+     return 12*math.cos(k)-5*/
+     Math.cos(2*k)-2*/
+          math.cos(3*k)-/
+          math.cos(4*k)
+speed(0)
+bgcolor("black")
+for i in range (10000):
+        goto (hearta (i) *20, heartb(i)*20)
+        for j in range (5):
+                color("#f73487")
+        goto(0,0)
+done()
+
+    
+
+
